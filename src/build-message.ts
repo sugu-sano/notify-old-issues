53611cf5650d1926a1eaa077cb093a2d6df0b6b4
nossa <sa.50.00.no.riku+github@gmail.com>
2020-11-28 22:46:14 +0900

    fix notification message template

--- src/build-message.ts
+++ src/build-message.ts
@@ -13,19 +13,20 @@ export function build_message(
     '[title]\n' +
     `以下の issue は最初の open から ${valid_days} 日以上経過しています` +
     '[/title]' +
-    Object.entries(group_by(expired_issues, (i) => i.user.login)).map(
-      ([user, issues]) =>
-        `${get_chatwotk_name(mapping, user)}\n` +
-        issues.reduce(
-          (acc, cur) =>
-            acc +
-            `title: ${cur.title}\n` +
-            `html_url: ${cur.html_url}\n` +
-            `created_at: ${cur.created_at.toString()}\n` +
-            '\n'
-        , "") +
-        '[hr]\n'
-    ) +
+    Object.entries(group_by(expired_issues, (i) => i.user.login))
+      .map(
+        ([user, issues]) =>
+          `${get_chatwotk_name(mapping, user)}\n` +
+          issues
+            .map(
+              (i) =>
+                `title: ${i.title}\n` +
+                `html_url: ${i.html_url}\n` +
+                `created_at: ${i.created_at.toString()}\n`
+            )
+            .join('\n')
+      )
+      .join('\n[hr]\n') +
     '[/info]\n' +
     '\n' +
     'Powered by notify-old-issues https://github.com/sugu-sano/notify-old-issues'
